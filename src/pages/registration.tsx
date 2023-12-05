b08ed38048ad4b0cb3ac0ded409023f18dfd3f00
aniltimalsina <timalsinaanil96@gmail.com>
2023-12-04 22:36:29 -0500

    modify categories items based on server data

--- src/pages/registration.tsx
+++ src/pages/registration.tsx
@@ -5,72 +5,73 @@ const Registration = () => {
   return (
     <div>
       <Header />
-      <div className="container mx-auto py-8 min-h-screen">
-        <div className="bg-white p-8 rounded-md shadow-md max-w-md w-full">
-          <h2 className="text-2xl font-semibold mb-6 text-center">
-            Create an Account
-          </h2>
+      <div className="container mx-auto grid items-center py-8 min-h-screen">
+        <div className="mx-auto py-8">
+          <div className="bg-white p-8 rounded-md shadow-md max-w-md w-full">
+            <h2 className="text-2xl font-semibold mb-6 text-center">
+              Create an Account
+            </h2>
+            <form>
+              <div className="mb-4">
+                <label
+                  htmlFor="username"
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                >
+                  Username
+                </label>
+                <input
+                  type="text"
+                  id="username"
+                  name="username"
+                  className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                />
+              </div>
 
-          <form>
-            <div className="mb-4">
-              <label
-                htmlFor="username"
-                className="block text-gray-700 text-sm font-bold mb-2"
+              <div className="mb-4">
+                <label
+                  htmlFor="email"
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                />
+              </div>
+
+              <div className="mb-6">
+                <label
+                  htmlFor="password"
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="bg-blue-500 text-white p-2 rounded-full w-full hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue"
               >
-                Username
-              </label>
-              <input
-                type="text"
-                id="username"
-                name="username"
-                className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-              />
-            </div>
+                Register
+              </button>
+            </form>
 
-            <div className="mb-4">
-              <label
-                htmlFor="email"
-                className="block text-gray-700 text-sm font-bold mb-2"
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-              />
-            </div>
-
-            <div className="mb-6">
-              <label
-                htmlFor="password"
-                className="block text-gray-700 text-sm font-bold mb-2"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-              />
-            </div>
-
-            <button
-              type="submit"
-              className="bg-blue-500 text-white p-2 rounded-full w-full hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue"
-            >
-              Register
-            </button>
-          </form>
-
-          <p className="mt-4 text-center text-gray-700">
-            Already have an account?
-            <Link to="/login" className="text-blue-500 hover:underline">
-              Login here
-            </Link>
-          </p>
+            <p className="mt-4 text-center text-gray-700">
+              Already have an account?
+              <Link to="/login" className="text-blue-500 hover:underline">
+                Login here
+              </Link>
+            </p>
+          </div>
         </div>
       </div>
       <Footer />
