2f80e2af0909e17b629368eaf4dbaccce2eba785
aniltimalsina <timalsinaanil96@gmail.com>
2023-12-05 19:46:14 -0500

    fetch product items with their details to the product page

--- src/features/productsSlice.tsx
+++ src/features/productsSlice.tsx
@@ -1,9 +1,36 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchProducts = createAsyncThunk(
+  "products/fetchProducts",
+  async () => {
+    const response = await axios.get("http://localhost:3000/storeData");
+    return response.data;
+  }
+);
 
 const productSlice = createSlice({
   name: "products",
-  initialState: {},
+  initialState: {
+    items: [],
+    status: "idle",
+    error: null,
+  },
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProducts.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.items = action.payload;
+      })
+      .addCase(fetchProducts.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
+  },
 });
 
 export default productSlice.reducer;
